94a7c36c8dff0db9295dfb337616272e9886aad7
Mrthomas20121 <thomas77300@gmail.com>
2021-08-01 14:52:28 +0200

    Create test.js

---
+++ test.js
@@ -0,0 +1,8 @@
+const VoidLogger = require('.')
+
+let logger = new VoidLogger('latest').setLogLocation('.').shouldCheckForNull();
+
+let d = new Date()
+
+logger.clear()
+logger.info(['test', 'test1'])
